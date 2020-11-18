2487e2a9c10d72de8550618a10689a2a493b995d
Dustin Belen <dustinbelen@gmail.com>
2020-11-18 12:32:33 -0600

    added custom fonts

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,13 +1,13 @@
 class ProductsController < ApplicationController
   def index
     @np = [] # New products to show in the home page
-    Product.includes(:category, :state).each do |product|
+    Product.includes(:category, :state, :product_images).each do |product|
       @np << product if product.state.name == "New"
     end
   end
 
   def all_products
-    @all_products = Product.includes(:category)
+    @all_products = Product.includes(:category, :product_images)
   end
 
   def about; end
