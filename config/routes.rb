abb61f8c8a585f81414cfeb9f61d149d1a4cd353
Dustin Belen <dustinbelen@gmail.com>
2020-11-25 07:33:46 -0600

    generated pages controller, rendered it to the home page, and started making the show page for each page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html\
+
+  resources :pages do
+    collection do
+      get "all_pages"
+      get "show"
+    end
+  end
 
   resources :categories do
     collection do
