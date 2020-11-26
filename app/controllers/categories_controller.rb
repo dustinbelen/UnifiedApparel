1656a58dc507cf9da90b14d86eb3055c2b47ed9b
Dustin Belen <dustinbelen@gmail.com>
2020-11-26 09:38:30 -0600

    fixed search bar issue to query the correct way

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -8,6 +8,18 @@ class CategoriesController < ApplicationController
   end
 
   def search
-    @searched_categories = Category.includes(:products).where("name LIKE ?", "%#{params[:search_term]}%")
+    @searched_products = []
+    if params[:select_category] == ""
+      @query = Product.where("name LIKE ?", "%#{params[:search_product]}%")
+      @query.each do |qp|
+        @searched_products << qp
+      end
+    else
+      @category_selected = Category.find(params[:select_category].to_i)
+      @query = @category_selected.products.where("name LIKE ?", "%#{params[:search_product]}%")
+      @query.each do |qp|
+        @searched_products << qp
+      end
+    end
   end
 end
